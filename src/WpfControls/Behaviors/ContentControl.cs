8fec5e82d3aac4d292b4393a47359d5642e9dd7f
Bill Tudor <bill@tudormobile.com>
2025-08-13 10:25:22 -0400

    Added contentcontrol transition behavior and new Card HeaderedContentControl (w/Footer). Closes #12

---
+++ src/WpfControls/Behaviors/ContentControl.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Tudormobile.Wpf.Behaviors;
+
+/// <summary>
+/// Content control behaviors (attached properties).
+/// <para>
+/// Transition property will apply a storyboard animation when the content property changes.
+/// </para>
+/// </summary>
+public static class ContentControl
+{
+    /// <summary>
+    /// Gets the Transition attached property.
+    /// </summary>
+    /// <param name="obj">Target object for the property.</param>
+    /// <returns>Transition storyboard.</returns>
+    public static Storyboard GetTransition(DependencyObject obj)
+    {
+        return (Storyboard)obj.GetValue(TransitionProperty);
+    }
+
+    /// <summary>
+    /// Sets the Transition attached property.
+    /// </summary>
+    /// <param name="obj">Target object for the property.</param>
+    /// <param name="value">Transition storyboard.</param>
+    public static void SetTransition(DependencyObject obj, Storyboard value)
+    {
+        obj.SetValue(TransitionProperty, value);
+    }
+
+    /// <inheritdoc/>
+    public static readonly DependencyProperty TransitionProperty = DependencyProperty
+        .RegisterAttached("Transition",
+        typeof(Storyboard),
+        typeof(ContentControl),
+        new PropertyMetadata(null, transitionPropertyChanged));
+
+    private static void transitionPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is System.Windows.Controls.ContentControl cc)
+        {
+            var descriptor = DependencyPropertyDescriptor
+                .FromProperty(System.Windows.Controls.ContentControl.ContentProperty,
+                 typeof(System.Windows.Controls.ContentControl));
+
+            descriptor.RemoveValueChanged(cc, contentPropertyChanged);
+            descriptor.AddValueChanged(cc, contentPropertyChanged);
+
+        }
+    }
+
+    private static void contentPropertyChanged(object? sender, EventArgs e)
+    {
+        if (sender is System.Windows.Controls.ContentControl cc)
+        {
+            var presenter = cc.Template?.FindName("PART_ContentPresenter", cc) as System.Windows.Controls.ContentPresenter;
+            if (presenter == null) presenter = EnumerateChildren(cc).OfType<System.Windows.Controls.ContentPresenter>().FirstOrDefault();
+            var storyboard = GetTransition(cc);
+            storyboard?.Begin(cc);
+        }
+    }
+
+    private static IEnumerable<DependencyObject> EnumerateChildren(DependencyObject root)
+    {
+        for (int index = 0; index < VisualTreeHelper.GetChildrenCount(root); index++)
+        {
+            var child = VisualTreeHelper.GetChild(root, index);
+            yield return child;
+            foreach (var c in EnumerateChildren(child)) yield return c;
+        }
+    }
+
+}
---
+++ src/WpfControls/Card.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Tudormobile.Wpf.Controls.Converters;
+
+namespace Tudormobile.Wpf.Controls;
+
+/// <summary>
+/// A headered content control with optional footer, corner radius, and content animation.
+/// <para>
+/// Animation is applied to the content presenter when the content changes. A number of
+/// built-in animations are available, or you can define your own.
+/// </para>
+/// </summary>
+public class Card : HeaderedContentControl
+{
+    static Card()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(Card), new FrameworkPropertyMetadata(typeof(Card)));
+    }
+
+    /// <summary>
+    /// Gets or sets the data used for the header of each control.
+    /// </summary>
+    public object? Footer
+    {
+        get { return (object)GetValue(FooterProperty); }
+        set { SetValue(FooterProperty, value); }
+    }
+
+    /// <inheritdoc/>
+    public static readonly DependencyProperty FooterProperty = DependencyProperty
+        .Register("Footer",
+        typeof(object),
+        typeof(Card),
+        new PropertyMetadata(null));
+
+    /// <summary>
+    /// Gets a value that indicates whether the header is null.
+    /// </summary>
+    public bool HasFooter
+    {
+        get { return (bool)GetValue(HasFooterProperty); }
+        set { SetValue(HasFooterProperty, value); }
+    }
+
+    /// <inheritdoc/>
+    public static readonly DependencyProperty HasFooterProperty = DependencyProperty
+        .Register("HasFooter",
+        typeof(bool),
+        typeof(Card),
+        new PropertyMetadata(null));
+
+    /// <summary>
+    /// Gets or sets a value that represents the degree to which the corners of a Border are rounded.
+    /// </summary>
+    public CornerRadius CornerRadius
+    {
+        get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+
+    /// <inheritdoc/>
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty
+        .Register("CornerRadius",
+        typeof(CornerRadius),
+        typeof(Card),
+        new PropertyMetadata(new CornerRadius(0)));
+
+    /// <summary>
+    /// Gets or sets the storyboard that defines the transition animation.
+    /// </summary>
+    /// <remarks>The <see cref="Storyboard"/> should be configured to define the desired visual
+    /// transition effect.  Ensure that the storyboard is properly initialized and contains the necessary animations
+    /// for the transition to function as expected.</remarks>
+    [TypeConverter(typeof(CardTransitionTypeConverter))]
+    public Storyboard Transition
+    {
+        get { return (Storyboard)GetValue(TransitionProperty); }
+        set { SetValue(TransitionProperty, value); }
+    }
+
+    /// <inheritdoc/>
+    public static readonly DependencyProperty TransitionProperty = DependencyProperty
+        .Register("Transition",
+        typeof(Storyboard),
+        typeof(Card),
+        new PropertyMetadata(null));
+
+    /// <inheritdoc/>
+    protected override void OnContentChanged(object oldContent, object newContent)
+    {
+        if (Transition != null)
+        {
+            // If the new content is a UIElement, we can apply the transition.
+            // This assumes that the Transition storyboard is set up to animate the ContentPresenter.
+            // You may need to adjust this logic based on your specific requirements.
+            if (Template?.FindName("PART_ContentPresenter", this) is ContentPresenter presenter)
+            {
+                Transition.Begin(presenter);
+            }
+            else if (newContent is FrameworkElement fe && ((UIElement)newContent).RenderTransform is TranslateTransform) //&& fe.RenderTransform is TranslateTransform)
+            {
+                Transition.Begin(fe);
+            }
+        }
+        base.OnContentChanged(oldContent, newContent);
+    }
+}
+
